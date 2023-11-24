aca6c5cd0f57ad8ac5c616ea22c2fab60550d929
wangwenjun <34988153@qq.com>
2023-11-24 20:47:07 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,54 +1,69 @@
 import './App.css';
-import {TodoList} from "./components/todoList/TodoList";
 import {Component} from "react";
+import {Todo} from "./components/todo/Todo";
 
 export default class App extends Component {
+    state = {
+        'todoList': [{
+            id: 1,
+            name: 'todo1',
+            isDone: false,
+        }, {
+            id: 2,
+            name: 'todo2',
+            isDone: false,
+        }, {
+            id: 3,
+            name: 'todo3',
+            isDone: true,
+        }]
+    };
 
-  state = {
-    'todoList': [{
-      id: 1,
-      name: 'todo1',
-      isDone: false,
-    }, {
-      id: 2,
-      name: 'todo2',
-      isDone: false,
-    }, {
-      id: 3,
-      name: 'todo3',
-      isDone: true,
-    }]
-  };
+    handleKeyUp = (event) => {
+        const {keyCode, target} = event;
 
-  handleKeyUp = (event) => {
-    const {keyCode, target} = event;
+        if (keyCode !== 13) return;
 
-    if (keyCode != 13) return;
+        if (target.value.trim() == '') return;
 
-    if (target.value.trim() == '') return;
+        const newTodoList = [{
+            id: this.state.todoList.length + 1,
+            name: target.value,
+            isDone: false,
+        }, ...this.state.todoList];
 
-    const newTodoList = [{
-      id: this.state.todoList.length + 1,
-      name: target.value,
-      isDone: false,
-    }, ...this.state.todoList];
+        this.setState({todoList: newTodoList});
 
-    this.setState({todoList: newTodoList});
+        target.value = '';
+    }
 
-    target.value = '';
-  }
+    changeTodo = (id, isDone) => {
+        let newTodoList = this.state.todoList.map(e => {
+            if (e.id == id) {
+                e.isDone = isDone;
+            }
+            return e;
+        });
+        this.setState({todoList: newTodoList});
+    }
 
-  render() {
-    return (
-        <div className="App">
+    render() {
+        return (
+            <div className="App">
 
-          <input onKeyUp={this.handleKeyUp} className={'textInput'} placeholder={'请输入任务名称，按回车确认'}/>
+                <input onKeyUp={this.handleKeyUp} className={'textInput'} placeholder='请输入任务名称，按回车确认'/>
 
-          <TodoList todoList={this.state.todoList}/>
+                <div className={'todoListContainer'}>
+                    {
+                        this.state.todoList.map(todo => {
+                            return <Todo todo={todo} key={todo.id} changeTodo={this.changeTodo}></Todo>
+                        })
+                    }
+                </div>
 
-        </div>
-    );
-  }
+            </div>
+        );
+    }
 
 }
 
--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -2,29 +2,37 @@ import {Component} from "react";
 import './todo.css'
 
 export class Todo extends Component {
-  state = {
-    mouse: false,
-  }
-
-  handleMouseLeave = (flag) => {
-    return () => {
-      this.setState({mouse: flag});
+    state = {
+        mouse: false,
     }
-  }
 
-  render() {
-    const {todo} = this.props;
+    handleMouseLeave = (flag) => {
+        return () => {
+            this.setState({mouse: flag});
+        }
+    }
 
-    return (
-        <div className={'todo'}
-             style={{backgroundColor: this.state.mouse ? '#ddd' : '#fff'}}
-             onMouseLeave={this.handleMouseLeave(false)} onMouseEnter={this.handleMouseLeave(true)}
-             key={todo.id}>
-          <input style={{cursor: 'pointer'}} type={'checkbox'} defaultChecked={todo.isDone}/>
-          <span>{todo.name}</span>
+    handleCheck = (id) => {
+        return (event) => {
+            const {changeTodo} = this.props;
+            changeTodo(id, event.target.checked);
+        }
+    }
 
-          <button style={{display: this.state.mouse ? 'block' : 'none'}} className={'btn'}>删除</button>
-        </div>
-    )
-  }
+    render() {
+        const {todo} = this.props;
+
+        return (
+            <div className={'todo'}
+                 style={{backgroundColor: this.state.mouse ? '#ddd' : '#fff'}}
+                 onMouseLeave={this.handleMouseLeave(false)} onMouseEnter={this.handleMouseLeave(true)}
+                 key={todo.id}>
+                <input style={{cursor: 'pointer'}} type={'checkbox'} defaultChecked={todo.isDone}
+                       onChange={this.handleCheck(todo.id)}/>
+                <span>{todo.name}</span>
+
+                <button style={{display: this.state.mouse ? 'block' : 'none'}} className={'btn'}>删除</button>
+            </div>
+        )
+    }
 }
