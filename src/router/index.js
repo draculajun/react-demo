10426216ab2f61721045d90c0c0e934774d13905
wangwenjun <34988153@qq.com>
2023-12-06 15:35:38 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import {createBrowserRouter, Navigate} from "react-router-dom";
+import HelloWorld from "../pages/helloWorld/HelloWorld";
+import App from "../App";
+import HelloWorld1 from "../pages/helloWorld/HelloWorld1";
+import HelloWorld2 from "../pages/helloWorld/HelloWorld2";
+import Todo from "../components/todo/Todo";
+import HelloWorld3 from "../pages/helloWorld/HelloWorld3";
+import ErrorPage from "../pages/ErrorPage";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Navigate to="/hello"/>
+    }, {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+                path: 'hello',
+                element: <Navigate to="/hello/hello1"/>
+            }, {
+                path: "hello",
+                element: <HelloWorld/>,
+                children: [
+                    {
+                        path: "hello1",
+                        element: <HelloWorld1/>,
+                    }, {
+                        path: "hello2/:id",
+                        element: <HelloWorld2/>,
+                    }, {
+                        path: "hello3",
+                        element: <HelloWorld3/>,
+                    }
+                ]
+            }, {
+                path: "todo",
+                element: <Todo/>,
+            }
+        ]
+    }, {
+        path: "*",
+        element: <ErrorPage/>,
+    },
+]);
+
+export default router;
